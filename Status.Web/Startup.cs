6840059c6b562da5af65f89b1a6c059bed2a6b48
silashn <silashn@kragghc.com>
2019-04-20 04:48:16 +0200

    Added realtime chart updating

--- Status.Web/Startup.cs
+++ Status.Web/Startup.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Status.Configuration.Database;
+using System;
 
 namespace Status.Web
 {
@@ -18,7 +14,7 @@ namespace Status.Web
         public Startup(IHostingEnvironment env)
         {
             IConfigurationBuilder builder = new ConfigurationBuilder()
-                .SetBasePath(AppContext.BaseDirectory)
+                .SetBasePath(AppContext.BaseDirectory + (env.IsDevelopment() ? "../../../" : ""))
                 .AddJsonFile("Config/AppSettings.json")
                 .AddEnvironmentVariables();
 
@@ -37,7 +33,10 @@ namespace Status.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
                 app.UseDeveloperExceptionPage();
+            }
 
             app.UseStaticFiles();
             app.UseMvc(options =>
